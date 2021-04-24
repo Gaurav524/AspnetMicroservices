2e5140b08ad681e7d9a33a0b170b85f5f9c6f499
Gaurav Gupta <GGUPTA@volvocars.com>
2021-04-24 17:44:05 +0200

    Added repository implementation and email service.

--- src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrderListQueryHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrderListQueryHandler.cs
@@ -24,7 +24,7 @@ namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
         public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, 
             CancellationToken cancellationToken)
         {
-           var orderList = await _orderRepository.GetOrderByUserName(request.UserName);
+           var orderList = await _orderRepository.GetOrdersByUserName(request.UserName);
            return _mapper.Map<List<OrdersVm>>(orderList);
         }
     }
