2da67560c0b06a4c3eabb22344f5927c06dce994
Gaurav Gupta <GGUPTA@volvocars.com>
2021-05-04 21:53:22 +0200

    Added controller

--- src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Shopping.Aggregator.Models;
 
 namespace Shopping.Aggregator.Services
 {
@@ -16,9 +16,10 @@ namespace Shopping.Aggregator.Services
             _client = client ?? throw new ArgumentNullException(nameof(client));
         }
 
-        public Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
         {
-            throw new NotImplementedException();
+            var response = await _client.GetAsync($"/api/v1/Order/{userName}");
+            return await response.ReadContentAs<List<OrderResponseModel>>();
         }
     }
 }
