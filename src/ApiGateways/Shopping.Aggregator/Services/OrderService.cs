d52b040d93f6b142626471e1756458f4d83c91dc
Gaurav Gupta <GGUPTA@volvocars.com>
2021-05-04 20:56:51 +0200

    Services created to connect with internal microservices.

---
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _client;
+
+        public OrderService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
